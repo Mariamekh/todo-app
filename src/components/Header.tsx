835a1e2dcd20ce616f879adab9a63e0aa17ebba8
mariamekh <marimekhrishvili11@gmail.com>
2025-02-11 20:52:07 +0400

    feat: implement task and history pages

---
+++ src/components/Header.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { CalendarDaysIcon, ClockIcon } from '@heroicons/react/24/solid';
+
+const Header = ({ onClear, title }) => {
+  const router = useRouter();
+  const currentPath = router.pathname;
+
+  return (
+    <div className="w-full max-w-2xl flex items-end justify-between pb-2 border-b border-[rgba(48,80,125,0.3)] mb-6">
+      <div className="flex space-x-3">
+        <button
+          onClick={() => router.push('/')}
+          className={`flex flex-col items-center space-y-1 focus:outline-none ${
+            currentPath === '/'
+              ? 'text-[rgba(48,80,125,1)] font-semibold'
+              : 'text-gray-500'
+          }`}
+        >
+          <span className="text-base">Tasks</span>
+          <div
+            className={`p-4 rounded-md flex items-center justify-center w-14 h-14 transition ${
+              currentPath === '/' ? 'bg-[rgba(106,108,224,1)]' : 'bg-gray-300'
+            }`}
+          >
+            <CalendarDaysIcon className="w-10 h-10 text-white" />
+          </div>
+        </button>
+
+        <button
+          onClick={() => router.push('/history')}
+          className={`flex flex-col items-center space-y-1 focus:outline-none ${
+            currentPath === '/history'
+              ? 'text-[rgba(48,80,125,1)] font-semibold'
+              : 'text-gray-500'
+          }`}
+        >
+          <span className="text-base">History</span>
+          <div
+            className={`p-4 rounded-md flex items-center justify-center w-14 h-14 transition   ${
+              currentPath === '/history'
+                ? 'bg-[rgba(106,108,224,1)]'
+                : 'bg-gray-300'
+            }`}
+          >
+            <ClockIcon className="w-10 h-10 text-white" />
+          </div>
+        </button>
+      </div>
+
+      <button
+        onClick={onClear}
+        className="text-[rgba(48,80,125,1)] underline font-medium text-sm hover:text-gray-700 transition "
+      >
+        {title}
+      </button>
+    </div>
+  );
+};
+
+export default Header;
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
-import { PencilIcon, TrashIcon, CheckCircleIcon, ChevronUpIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
+import React, { useState } from 'react';
+import {
+  PencilIcon,
+  TrashIcon,
+  CheckCircleIcon,
+  ChevronUpIcon,
+  ChevronDownIcon,
+} from '@heroicons/react/24/outline';
 
-const TaskCard = ({ task, onEdit, onDelete, onMarkCompleted }) => {
+const TaskCard = ({
+  task,
+  onEdit,
+  onDelete,
+  onMarkCompleted,
+  isHistory = false,
+}) => {
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <div className="bg-[rgba(246,250,255,1)] rounded-lg shadow-md w-full md:w-1/2 lg:w-1/3 flex flex-col p-4 transition-all duration-300">
-       <div className="flex justify-between items-center">
-        <h3 className="font-poppins font-medium text-[rgba(48,80,125,1)] text-lg md:text-xl">
-          {task.title}
-        </h3>
+    <div className="bg-[rgba(246,250,255,1)] rounded-lg shadow-md w-full max-w-[480px] flex flex-col p-4 transition-all duration-300">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center space-x-2 w-full">
+        <h3
+            className="font-poppins font-medium text-[rgba(48,80,125,1)] text-lg md:text-xl truncate w-[85%] md:w-[90%] lg:w-[82%] break-words leading-tight"
+            title={task.title} 
+          >
+            {task.title}
+          </h3>
+
+          {isHistory && !expanded && (
+            <span className="text-[rgba(108,134,168,1)] flex items-center space-x-1 text-xs md:text-sm font-medium font-poppins">
+              <CheckCircleIcon className="w-6 h-6 font-bold text-[rgba(61,203,101,1)]" />
+            </span>
+          )}
+        </div>
         <button onClick={() => setExpanded(!expanded)}>
           {expanded ? (
             <ChevronUpIcon className="w-5 h-5 text-gray-500" />
@@ -19,33 +42,48 @@ const TaskCard = ({ task, onEdit, onDelete, onMarkCompleted }) => {
         </button>
       </div>
 
-       <div
-        className={`transition-all duration-300 overflow-hidden ${
-          expanded ? "max-h-40 opacity-100 py-2" : "max-h-0 opacity-0 py-0"
-        }`}
+      <div
+        className={`transition-all duration-300 overflow-hidden ${expanded ? 'max-h-40 opacity-100 py-2' : 'max-h-0 opacity-0 py-0'}`}
       >
         <p className="bg-[rgba(232,241,253,1)] text-[rgba(108,134,168,1)] font-medium text-sm md:text-base p-2 rounded-md font-poppins shadow-[inset_1px_1px_4px_0px_rgba(48,80,125,0.25)]">
-          {task.description || "No description provided."}
+          {task.description || 'No description provided.'}
         </p>
       </div>
 
-       <div className="mt-4 flex justify-between items-center">
-         <div className="flex space-x-3">
-          <button onClick={() => onEdit(task)} className="hover:scale-105 transition">
-            <PencilIcon className="w-5 h-5 text-[rgba(55,47,47,1)]" />
-          </button>
-          <button onClick={() => onDelete(task.id)} className="hover:scale-105 transition">
-            <TrashIcon className="w-5 h-5 text-[rgba(244,134,134,1)]" />
-          </button>
+      <div className="mt-4 flex justify-between items-center">
+        <div className="flex space-x-3">
+          {!isHistory && (
+            <button
+              onClick={() => onEdit(task)}
+              className="hover:scale-105 transition"
+            >
+              <PencilIcon className="w-5 h-5 text-[rgba(55,47,47,1)]" />
+            </button>
+          )}
+          {!isHistory || (isHistory && expanded) ? (
+            <button
+              onClick={() => onDelete(task.id)}
+              className="hover:scale-105 transition"
+            >
+              <TrashIcon className="w-5 h-5 text-[rgba(244,134,134,1)]" />
+            </button>
+          ) : null}
         </div>
 
-         <button
-          onClick={() => onMarkCompleted(task.id)}
-          className="text-[rgba(108,134,168,1)] flex items-center space-x-1 hover:text-opacity-80 transition text-xs md:text-sm font-medium font-poppins"
-        >
-          <span>Mark completed</span>
-          <CheckCircleIcon className="w-5 h-5 text-[rgba(61,203,101,0.6)]" />
-        </button>
+        {isHistory && expanded ? (
+          <span className="text-[rgba(108,134,168,1)] flex items-center space-x-1 text-xs md:text-sm font-medium font-poppins">
+            <span>Completed</span>
+            <CheckCircleIcon className="w-6 h-6 font-bold text-[rgba(61,203,101,1)]" />
+          </span>
+        ) : !isHistory ? (
+          <button
+            onClick={() => onMarkCompleted(task.id)}
+            className="text-[rgba(108,134,168,1)] flex items-center space-x-1 hover:text-opacity-80 transition text-xs md:text-sm font-medium font-poppins"
+          >
+            <span>Mark completed</span>
+            <CheckCircleIcon className="w-6 h-6 font-bold text-[rgba(61,203,101,1)]" />
+          </button>
+        ) : null}
       </div>
     </div>
   );
