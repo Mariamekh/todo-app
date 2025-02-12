5d70208c656acba7c3837ff28488e5884aeb6cd7
mariamekh <marimekhrishvili11@gmail.com>
2025-02-12 16:02:16 +0400

    refactor: add env for test project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,27 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname, 
 });
 
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+export default [
+  ...compat.extends(
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier'
+  ),
+  {
+    plugins: ['@typescript-eslint', 'react'],
+    rules: {
+      'react/react-in-jsx-scope': 'off', 
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
+      'no-console': 'warn',
+      'prefer-const': 'warn',
+    },
+  },
 ];
-
-export default eslintConfig;
